eed3e22f6b47179e2d606add46a903e95ff8767d
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-04-19 13:01:26 -0400

    Added axios module and fetched music library data.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+
+  useEffect(() => {
+    makeGetLibraryRequest();
+  }, []);
+
+  async function makeGetLibraryRequest() {
+    try {
+      let response = await axios.get('http://www.devcodecampmusiclibrary.com/api/music');
+      console.log(response.data);
+    } catch(err) {
+      console.log(err.message);
+    }
+  }
 
   return (
     <div className="App">
@@ -9,5 +24,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
