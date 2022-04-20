ebe4e09e409428aa94c93cfb88c0fb4e64810329
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-04-20 13:01:25 -0400

    SearchBar component now finds matches with partial string for album title.

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -5,33 +5,32 @@ const SearchBar = (props) => {
 
     let textInput = React.createRef();  // React use ref to get input value
 
-    let onOnclickHandler = (e) => {
-        console.log(textInput.current.value); 
-    };
+    let handleSearch = (event) => {
+        let searchStr = textInput.current.value;
+        let foundMedia = props.mediaData.filter(function (potentialMatch) {
+            //x.title.toLowerCase().includes(searchStr.toLowerCase())
+            //if (potentialMatch.album === textInput.current.value) {
+            if (potentialMatch.album.toLowerCase().includes(searchStr.toLowerCase())) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
 
-    //handleSearch(songs) {
-    //  
-    //    let foundMedia = songs.filter(function (potentialMatch) {
-    //      if (potentialMatch.album === songs.album) {
-    //        return true;
-    //      }
-    //      else {
-    //        return false;
-    //      }
-    //    });
-    //  
-    //    console.log(foundMedia);
-    //    return foundMedia;
-    //  }
-      
+        console.log(textInput.current.value);
+        //console.log(props.mediaData);
 
+        console.log(foundMedia);
+        return foundMedia;
+    }
 
     return (
 
         <div id='search-bar'>
             <label id='search-label'>Find Song</label>
             <input id='search-input' ref={textInput} type='text' placeholder='Search by: Song Title, Ablum Name, Artist or Group Name, or Release Date'></input>
-            <button id='search-button' onClick={onOnclickHandler}>Search</button>
+            <button id='search-button' onClick={handleSearch}>Search</button>
         </div>
 
     );
